3e1010685dc59617b0e4304ff2f9ecf49a250f4a
ayushkrsingh <ayushks070707@gmail.com>
2026-07-30 12:56:44 +0530

    Backend setup

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+        console.log(`MongoDB connected !!! ${connectionInstance.connection.host}`);
+        
+    } catch (error) {
+        console.log("MongoDB connection Failed !", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
